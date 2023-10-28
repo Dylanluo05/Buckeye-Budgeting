1c3e75df92105f0a515a88233631b8f23ef08089
Dylan Luo <dragonfly.luo@gmail.com>
2023-10-28 13:54:42 -0400

    Ping.vue

--- BuckeyeBudgeting/src/router/index.ts
+++ BuckeyeBudgeting/src/router/index.ts
@@ -1,10 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+//import HomeView from '../views/HomeView.vue'
+import Ping from '../components/Ping.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/ping',
+      name: 'ping',
+      component: Ping
+    }
+    /*{
       path: '/',
       name: 'home',
       component: HomeView
@@ -16,7 +22,7 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
-    }
+    }*/
   ]
 })
 
