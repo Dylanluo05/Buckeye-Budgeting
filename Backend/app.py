89de2378aaa8a06328ec4f518dd711e74e97fc38
caelummmuhhh <caelu2.0@gmail.com>
2023-10-28 13:55:45 -0400

    init python

--- Backend/app.py
+++ Backend/app.py
@@ -1,22 +1,34 @@
 import pandas as pd
 from flask_cors import CORS
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, make_response
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 CORS(app, resources={r'/*': {'origins': '*'}})
 
 
-def load_file_to_df(file_path: str) -> pd.DataFrame:
-    if file_path[:-4] == '.csv':
-        return pd.read_csv(file_path)
-    else:
-        raise 
+def load_csv_to_df(file_path: str) -> pd.DataFrame:
+    return pd.read_csv(file_path)
+
+
 
 @app.route('/test', methods=['GET'])
 def get_test():
     return '<p>Hello World</p>'
 
 
+@app.route('/user_info', methods=['GET'])
+def get_basic_user_info():
+    username = request.args.get('username')
+    password = request.args.get('password') # ideally this would be encrypted and stored differently
+
+    
+
+    pass
+
+
+
+
 if __name__ == '__main__':
     app.run()
+
